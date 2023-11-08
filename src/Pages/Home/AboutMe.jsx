a97b2aa5c67822a21137106380d23bb0b6fd22a3
Francis Ngo <francis.ngo@protonmail.com>
2023-11-07 22:20:15 -0800

    final

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,39 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          https://resume-francis-ngo.tiiny.site/
+          https://www.linkedin.com/in/francis-ngo-214bb4279/
+          https://francis-ngo1.github.io/
+          Hello, I'm Francis, and I'm a dynamic professional with a unique 
+          blend of skills and a passion for driving innovation in the tech world.
+          But what truly sets me apart are my strengths: humor, creativity, bravery, 
+          leadership, and a keen perspective. These traits have been my guiding light 
+          in every role I've undertaken. With background in operational management 
+          as a front office manager and even managing an axe throwing bar, I've honed my 
+          leadership skills and a knack for creative problem-solving. My bachelor's degree 
+          in finance from Penn State University adds a strategic layer to my 
+          thinking, allowing me to approach coding challenges with a business-oriented mindset.
+          
+          </p>
+          <p className="hero--section-description">
+          What makes me unique is the diverse skill set I bring to the table. Not only do I 
+          excel in software engineering, but I also possess proficiency in data science, machine 
+          learning, and game development with Python. This multifaceted background, combined with 
+          my management experience, allows me to deliver innovative solutions and provide the leadership 
+          required to drive projects to success.  I'm a creative, strategic thinker with the 
+          courage to tackle challenges head-on, and I'm ready to bring my unique perspective and 
+          expertise to your team to achieve great things.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,26 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Francis</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          https://resume-francis-ngo.tiiny.site/
+          https://www.linkedin.com/in/francis-ngo-214bb4279/
+          https://francis-ngo1.github.io/
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
